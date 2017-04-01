739c7a28929d08248da8816e607373297f08e57b
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2017-04-01 22:24:28 +0800

    upd

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req,res) {
+  res.render("views/index.html");
+})
+
+app.listen(3000, function(){
+  console.log("listening on port 3000")
+});
