180cf2bd43cc205de7b042760b838c1bad27fae6
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2017-04-03 15:11:47 +0800

    add service

--- public/JS/routes.js
+++ public/JS/routes.js
@@ -3,7 +3,7 @@ angularApp.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'pages/main.html',
-        controller:'mainController'
+        controller: 'mainController'
       })
       .when('/about', {
         templateUrl: 'pages/about.html',
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req,res) {
-  res.render("views/index.html");
+  res.render("index.html")
 })
+
+
+
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("listening on port 3000")
